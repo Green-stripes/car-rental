66d2b962aa2b28293bcf196e9c027f7133f0c13d
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-09-19 12:51:55 +0200

    completed index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Car.destroy_all
 
 20.times do
-  car = Car.new(model: ["Honda", "Audi", "Bmw", "Porsche", "Sports Car", "Vintage Car"].sample, price_per_day: rand(50 - 500))
+  car = Car.new(model: ["Honda", "Audi", "Bmw", "Porsche", "Mustang"].sample, price_per_day: rand(50 - 500))
   car.save
-  car.photo = "https://unsplash.com/s/photos/#{car.model}"
+  car.photo = "https://source.unsplash.com/featured/?#{car.model}"
   car.save
   puts "Created #{car.id} cars"
 end
