0d87fc52922b35508e18b6d7babb4d6923240d45
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-09-19 11:39:59 +0200

    created car model, controller and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Car.destroy_all
+
+20.times do
+  car = Car.new(model: ["Honda", "Audi", "Bmw", "Porsche", "Sports Car", "Vintage Car"].sample, price_per_day: rand(50 - 500))
+  car.save
+  car.photo = "https://unsplash.com/s/photos/#{car.model}"
+  car.save
+  puts "Created #{car.id} cars"
+end
